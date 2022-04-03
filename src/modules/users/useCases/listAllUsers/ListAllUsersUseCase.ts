b70a6b3514ecd122eb01934165fee2f3d283f92c
jailsonsf <jailson.s.f@outlook.com>
2022-04-03 17:26:08 -0300

    Update ListAllUsers UseCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,22 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User don't exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("User not Admin");
+    }
+
+    const users: User[] = this.usersRepository.list();
+    return users;
   }
 }
 
