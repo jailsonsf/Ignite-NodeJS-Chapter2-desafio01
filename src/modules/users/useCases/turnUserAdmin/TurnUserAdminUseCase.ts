13e2b063e7d74eb537fc4156c03098893a96feb6
jailsonsf <jailson.s.f@outlook.com>
2022-04-03 16:54:37 -0300

    Update TurnUserAdmin UseCase

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,19 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User {
     // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExists) {
+      throw new Error("User don't exists");
+    }
+
+    const user = this.usersRepository.turnAdmin(userAlreadyExists);
+
+    return user;
   }
 }
 
